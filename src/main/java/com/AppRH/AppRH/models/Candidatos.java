0dfb8708611935541eae7c8785929ccf74acf964
Gilberto Junior <giljrsantos@gmail.com>
2026-07-07 19:24:35 -0300

    Criação da model Vaga

---
+++ src/main/java/com/AppRH/AppRH/models/Candidatos.java
@@ -0,0 +1,74 @@
+package com.AppRH.AppRH.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Candidatos {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotEmpty
+    @Column(unique = true)
+    private String rg;
+
+    private String nomeCandidato;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String telefone;
+
+    @ManyToOne
+    @JoinColumn(name = "vaga_id", referencedColumnName = "codigo")
+    private Vaga vaga;
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNomeCandidato() {
+        return nomeCandidato;
+    }
+
+    public void setNomeCandidato(String nomeCandidato) {
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Vaga getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Vaga vaga) {
+        this.vaga = vaga;
+    }
+}
