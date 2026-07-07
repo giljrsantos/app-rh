dfaa29f0b361d9bb82df5392834870b8f3cee156
Gilberto Junior <giljrsantos@gmail.com>
2026-07-07 19:24:54 -0300

    Criação da model Candidatos

---
+++ src/main/java/com/AppRH/AppRH/models/Vaga.java
@@ -0,0 +1,86 @@
+package com.AppRH.AppRH.models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import javax.validation.constraints.NotEmpty;
+
+
+@Entity
+public class Vaga implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigo;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String descricao;
+
+    @NotEmpty
+    private String data;
+
+    @NotEmpty
+    private String salario;
+
+    @OneToMany(mappedBy = "vaga", cascade = CascadeType.REMOVE)
+    private List<Candidatos> candidatos;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+
+    public List<Candidatos> getCandidatos() {
+        return candidatos;
+    }
+
+    public void setCandidatos(List<Candidatos> candidatos) {
+        this.candidatos = candidatos;
+    }
+}
