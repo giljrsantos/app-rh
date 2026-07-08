38b5f6069e567b8818637d3be401769924c3ed62
Gilberto Junior <giljrsantos@gmail.com>
2026-07-08 18:30:37 -0300

    Created CandidatoRepository

---
+++ src/main/java/com/AppRH/AppRH/repository/CandidatoRepository.java
@@ -0,0 +1,7 @@
+package com.AppRH.AppRH.repository;
+
+import com.AppRH.AppRH.models.Candidatos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidatoRepository extends JpaRepository<Candidatos, Long> {
+}
