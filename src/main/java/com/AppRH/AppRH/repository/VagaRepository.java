516ef6d5423bd0bc310cf3c4222778b7556ae0b2
Gilberto Junior <giljrsantos@gmail.com>
2026-07-08 18:29:48 -0300

    Created vaga repository

---
+++ src/main/java/com/AppRH/AppRH/repository/VagaRepository.java
@@ -0,0 +1,14 @@
+package com.AppRH.AppRH.repository;
+
+import com.AppRH.AppRH.models.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface VagaRepository extends CrudRepository<Vaga, String> {
+
+    Vaga findByCodigo(Long codigo);
+    List<Vaga> findByNome(String nome);
+
+}
